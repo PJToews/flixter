4e38b4d184cb03a872951ef9e146b8969b701adf
PJToews <lohasjean@gmail.com>
2019-05-02 02:37:26 +0000

    course image error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,14 @@ class StaticPagesController < ApplicationController
 
   def index
   end
+
+  def team
+  end
+
+  def career
+  end
+
+  def privacy
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'team', to: 'static_pages#team'
+  get 'career', to: 'static_pages#career'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
